83f1a0a32041f184874092dd7c9a9ab1681b7bb4
matherm2024 <matherm@cityschools.org.uk>
2024-12-09 13:35:36 +0000

    added about

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -8,9 +8,31 @@ const About = () => {
         <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
       </div>
       <div className='my-10 flex flex-col md:flex-row gap-12'>
-        <img className='w-full md:max-w-[360px] rounded-lg' src={assets.about_image} alt="" />
-        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
-          <p className='text-gray-800'>About information ... slot times, what to do if your feeling stressed/anxious ect</p>
+        <img className='w-full md:max-h-[300px] md:max-w-[450px] rounded-lg' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-3/4 text-sm text-gray-600'>
+          <p className='text-gray-600 '>The Counsellors offer a safe and <span className='text-gray-900 font-medium'>confidential </span> space where you can talk about anything - no problem is too big or small.
+
+Everything is welcome there is no judgement. If you are nervous to come alone, you are welcome to bring friends with you. <br/><br/>
+
+The Counsellors are all experienced, kind and approachable with the skills to listen deeply and help guide you to navigate difficult emotions and feelings or make challenging decisions. <br/><br/>
+
+We do not tell you what to do but support you to make the choices yourselves. This can be flipping the script on negative dogmatic thinking patterns transforming them into more flexible realistic and self accepting narratives.<br/>
+<br/>
+Some common themes in Counselling: <br/>
+
+Performance related anxieties <br/>
+Shame of being anxious<br/>
+Anger management<br/>
+Procrastination<br/>
+Sexuality<br/>
+Self worth<br/>
+Friendships/relationships<br/><br/>
+
+
+We are available on D floor, and you can book your session via the school portal any time of the day or night. We are available 5 days a week. <br/>
+
+If you need support outside of school hours, you may find the following links useful:
+</p>
         </div>
       </div>
     </div>
