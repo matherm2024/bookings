fe08ac9eadd6079046b6305c7980bb8c2c538150
matherm2024 <matherm@cityschools.org.uk>
2024-12-12 13:17:48 +0000

    Remove directory bookingfrontned from repository

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
+  server:{port:5173},
+  base: 'https://clsgcounsellorbookings-afg2a3dga9dkdeg7.uksouth-01.azurewebsites.net',
 })
+
