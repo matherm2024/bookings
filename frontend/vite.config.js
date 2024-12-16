344dd97520dce56566dd1f25af10a045d2f74f41
matherm2024 <matherm@cityschools.org.uk>
2024-12-16 13:25:21 +0000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173, // Local development port
   },
-  base: 'clsgcounsellorbookings-afg2a3dga9dkdeg7.uksouth-01.azurewebsites.net/', // Replace with your Azure URL
+  base: '/clsgcounsellorbookings-afg2a3dga9dkdeg7.uksouth-01.azurewebsites.net/', // Replace with your Azure URL
   build: {
     outDir: 'dist', // Ensure this matches your workflow configuration
   },
