1dc3cb857c510cd1032021f070f87cbe0bffcab2
matherm2024 <matherm@cityschools.org.uk>
2024-12-18 11:58:50 +0000

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
  
   server:{port:5174},
-  base: '/login'
+  base: '/'
 
 }
 
