c77d1752beb158d2c5ed00b1201258ef5d6f0ceb
matherm2024 <matherm@cityschools.org.uk>
2025-01-14 10:11:59 +0000

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
-  server:{port:5174},
-  base: '/'
-
-},
-
-)
+  server: {
+    port: 5174, // Specify the development server port
+  },
+  base: '/', // Base public path
+  define: {
+    'process.env': process.env, // Explicitly use process.env in case Vite defaults fail
+  },
+});
