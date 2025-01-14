824328e311c281fcc3c82280967877e2d02d4012
matherm2024 <matherm@cityschools.org.uk>
2025-01-14 11:06:05 +0000

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+    // Load environment variables based on the current mode
+  const env = loadEnv(mode, process.cwd());
+
   plugins: [react()],
   server: {
     port: 5174, // Specify the development server port
